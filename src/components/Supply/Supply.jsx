b7cf316dfdb6d3764ca6d004673d388259d290c9
Poblaaa <mahidulislamsohan76@gmail.com>
2025-06-23 09:25:17 +0600

    Supplier Images

--- src/components/Supply/Supply.jsx
+++ src/components/Supply/Supply.jsx
@@ -1,4 +1,8 @@
 import Container from "../Layout/Container";
+import supplier01 from '../../assets/supplier01.png'
+import supplier02 from '../../assets/supplier02.png'
+import supplier03 from '../../assets/supplier03.png'
+import supplier04 from '../../assets/supplier04.png'
 
 const Supply = () => {
 	return (
@@ -16,6 +20,12 @@ const Supply = () => {
 					</p>
 				</div>
 			</Container> 
+			<div className="flex justify-between gap-x-[30px]">
+				<img src={supplier01} alt="" />
+				<img src={supplier02} alt="" />
+				<img src={supplier03} alt="" />
+				<img src={supplier04} alt="" />
+			</div>
 		</div>
 	);
 };

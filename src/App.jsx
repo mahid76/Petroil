adf81f4f648412daed0527d0c49382b1521f9c50
Poblaaa <mahidulislamsohan76@gmail.com>
2025-06-23 09:17:33 +0600

    Supply  Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
+import Supply from './components/Supply/Supply'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
      <Header />
      <Navbar />
      <Banner />
+     <Supply />
     </>
   )
 }
---
+++ src/components/Supply/Supply.jsx
@@ -0,0 +1,23 @@
+import Container from "../Layout/Container";
+
+const Supply = () => {
+	return (
+		<div className="pt-[78px] pb-[100px]  ">
+			<Container>
+				<div className="flex justify-between items-center">
+					<h3 className="max-w-[289px] font-primary font-bold leading-[72px] text-[48px]   ">
+						The biggest supplier on the country
+					</h3>
+					<p className="max-w-[568px] font-primary font-medium text-[#6C6C6C] ">
+						It is a long established fact that a reader will be distracted by
+						the readable content of a page when looking at its layout. The point
+						of using Lorem Ipsum is that it has a more-or-less normal
+						distribution.
+					</p>
+				</div>
+			</Container>
+		</div>
+	);
+};
+
+export default Supply;

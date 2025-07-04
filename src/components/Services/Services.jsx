360c7abf02a731f45577b71d69b0d92a372322c7
Md. Mahidul Islam <mahidulislamsohan76@gmail.com>
2025-07-04 23:52:08 +0600

    Update Services.jsx

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
 	return (
 		<div className="mt-5">
 			<div className="flex">
-				<div className="w-1/2 flex flex-col items-end  justify-center pr-[149px]">
+				<div className="w-1/2 flex flex-col items-end  justify-center pr-[149px] ">
 				<h2 className="font-primary text-[64px] font-bold text-black ">Our Services</h2>
 				<p className="font-primary max-w-[405px]  font-medium text-secondary ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
 				</div>
